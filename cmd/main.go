804792a8647a021bb774b68e26844f4d15671d5e
tomspaven <tomspaven@gmail.com>
2019-05-16 08:29:22 -0700

    Added support for endpoint discovery

--- cmd/main.go
+++ cmd/main.go
@@ -1,47 +1,41 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"endpoint-visualiser-server/pkg/clienthandler/rest"
+	"endpoint-visualiser-server/pkg/keyboard"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
-type NestStruct struct {
-	NestData  string `json:"nestdata"`
-	NestData2 string `json:"othernestdata"`
-}
-
-func (ns NestStruct) String() string {
-	return fmt.Sprintf("nestData: %s, otherNestData: %s", ns.NestData, ns.NestData2)
-}
-
-type TestStruct struct {
-	Data     string     `json:"data"`
-	NestData NestStruct `json:"envelope"`
-}
-
-func (ts TestStruct) String() string {
-	return fmt.Sprintf("data: %s, nestData: %s", ts.Data, ts.NestData.String())
+var clients = make(map[*websocket.Conn]bool)
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func main() {
-	http.HandleFunc("/endpoints", func(w http.ResponseWriter, r *http.Request) {
-
-		w.Header().Add("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		testResponse := TestStruct{
-			Data: "Tommy",
-			NestData: NestStruct{
-				NestData:  "Spaven",
-				NestData2: "Whatever mate",
-			},
-		}
-		fmt.Printf("\nGot a request!")
-		fmt.Printf("\nSending response %s!", testResponse)
-		json.NewEncoder(w).Encode(testResponse)
-	})
+	router := mux.NewRouter()
+	router.HandleFunc("/endpoints", rest.EndpointDiscoveryHandler).Methods("GET")
+	router.HandleFunc("/ws", websocketsHandler)
+	_, _, _ = keyboard.GetChar()
 
 	fmt.Printf("\nListening on port 3031")
-	log.Fatal(http.ListenAndServe(":3031", nil))
+	log.Fatal(http.ListenAndServe(":3031", handlers.CORS(handlers.AllowedMethods([]string{"GET"}), handlers.AllowedOrigins([]string{"*"}))(router)))
+}
+
+func websocketsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// register client
+	clients[ws] = true
 }
