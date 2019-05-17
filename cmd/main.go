3239e1d3c41932e498e59e66aafe02e105397d4c
tomspaven <tomspaven@gmail.com>
2019-05-17 16:08:58 +0200

    Linked up keylogging, websockets with a simple endpoint mocker

--- cmd/main.go
+++ cmd/main.go
@@ -4,38 +4,48 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 
 	"endpoint-visualiser-server/pkg/clienthandler/rest"
+	"endpoint-visualiser-server/pkg/clienthandler/websocket"
+	"endpoint-visualiser-server/pkg/config"
+	"endpoint-visualiser-server/pkg/endpoint"
+	"endpoint-visualiser-server/pkg/event"
 	"endpoint-visualiser-server/pkg/keyboard"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
 )
 
-var clients = make(map[*websocket.Conn]bool)
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/endpoints", rest.EndpointDiscoveryHandler).Methods("GET")
-	router.HandleFunc("/ws", websocketsHandler)
-	_, _, _ = keyboard.GetChar()
 
-	fmt.Printf("\nListening on port 3031")
-	log.Fatal(http.ListenAndServe(":3031", handlers.CORS(handlers.AllowedMethods([]string{"GET"}), handlers.AllowedOrigins([]string{"*"}))(router)))
-}
-
-func websocketsHandler(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
+	config, err := config.ReadConfig()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Failed To Read Config")
+		os.Exit(1)
 	}
 
-	// register client
-	clients[ws] = true
+	eventChan := make(chan event.Event)
+
+	webSocketManager := websocket.New(websocket.WithClientRegisterer)
+	restManager := rest.New(config)
+
+	endpointManager := endpoint.NewManager(config, webSocketManager, eventChan)
+	keyListener, err := keyboard.NewListener(config, eventChan)
+	if err != nil {
+		fmt.Printf("KeyListener Startup Failed. Error: %s", err.Error())
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/endpoints", restManager.EndpointDiscoveryHandler).Methods("GET")
+	router.HandleFunc("/websocketRegistration", webSocketManager.Handler()).Methods("POST")
+
+	synchStart := &sync.WaitGroup{}
+	endpointManager.Start(synchStart)
+	keyListener.Start(synchStart)
+	synchStart.Wait()
+
+	fmt.Printf("\nListening on port 3031")
+	log.Fatal(http.ListenAndServe(":3031", handlers.CORS(handlers.AllowedMethods([]string{"GET", "POST"}), handlers.AllowedOrigins([]string{"*"}))(router)))
 }
